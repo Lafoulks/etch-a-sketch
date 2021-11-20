25c7e989e7b2f31b5a350f7d9dfbd69c87471966
Lafoulks <foulks.lauren@gmail.com>
2021-11-20 11:22:56 -0500

    added 16 rows and 16 cells for each row

--- myscript.js
+++ myscript.js
@@ -1,30 +1,22 @@
 const container = document.querySelector('#container');
 container.classList.add('container');
-container.textContent = 'Container';
-
-const row = document.createElement('div');
-row.classList.add('row');
-row.textContent = 'Row';
-
-
-const cell = document.createElement('div');
-cell.classList.add('cell');
-cell.textContent = 'Cell';
 
 function createRows () {
     for (i = 0; i < 16; i++) {
-        row.classList.add('row');
+        const row = document.createElement('div');
+        row.classList.add('row')
+        createCells(row);
         container.appendChild(row);
     }
 }
 
-function createGrid () {
+function createCells (row) {
     for (j = 0; j < 16; j++) {
+        const cell = document.createElement('div');
         cell.classList.add('cell');
         row.appendChild(cell);
     }
 }
 
 createRows();
-createGrid();
 
