14217f2762fa5245fe8c2825a1fb17a7fbcc2eab
Lafoulks <foulks.lauren@gmail.com>
2021-11-21 07:34:05 -0500

    added color change hover effect for grid

--- myscript.js
+++ myscript.js
@@ -1,6 +1,8 @@
+//Setting up container
 const container = document.querySelector('#container');
 container.classList.add('container');
 
+//Creating grid row and cells
 function createRows () {
     for (i = 0; i < 16; i++) {
         const row = document.createElement('div');
@@ -19,4 +21,21 @@ function createCells (row) {
 }
 
 createRows();
+
+/*Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
+
+Hint: “Hovering” is what happens when your mouse enters a div and ends when your mouse leaves it. 
+You can set up event listeners for either of those events as a starting point.
+There are multiple ways to change the color of the divs, including:
+    adding a new class to the div.
+    changing the div’s background color using JavaScript.
+*/
+
+let cells = document.querySelectorAll('.cell');
+cells.forEach((cell) => {
+    cell.addEventListener('mouseover', () => {
+        cell.style.backgroundColor = 'black';
+    });
+});
+
 
